2ffb00b23ac69de952fc102aab086d1e6a125c33
tuliosbhz <48711513+tuliosbhz@users.noreply.github.com>
2024-02-09 10:07:20 +0000

    added port to self address

--- benchmarks/benchmark_kpi.py
+++ benchmarks/benchmark_kpi.py
@@ -84,6 +84,7 @@ def perNodeBenchmark(requestsPerSecond, requestSize, numNodes, numNodesReadonly=
     allAddrs = read_ip_port_file("nodes_addrs.txt")
     print(allAddrs)
     selfAddr = ip_address_assign()
+    selfAddr = selfAddr + ":" + str(START_PORT)
     if selfAddr:
         addrs = allAddrs.remove(selfAddr)
     p = Popen(cmd + [selfAddr] + addrs, stdin=PIPE)
