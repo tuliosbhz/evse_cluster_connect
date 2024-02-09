216157e69f0541d62beb81e76bfcb2adf6266cf3
tuliosbhz <48711513+tuliosbhz@users.noreply.github.com>
2024-02-09 10:35:18 +0000

    counter_per_node

--- cluster_connect/counter.py
+++ cluster_connect/counter.py
@@ -6,7 +6,7 @@ import time
 from functools import partial
 sys.path.append("../")
 from pysyncobj import SyncObj, replicated
-
+from find_my_addr import ip_address_assign
 
 class TestObj(SyncObj):
 
@@ -26,14 +26,33 @@ class TestObj(SyncObj):
 def onAdd(res, err, cnt):
     print('onAdd %d:' % cnt, res, err)
 
+def read_ip_port_file(file_path):
+    ip_port_list = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            ip_port = line.strip()  # Remove leading/trailing whitespace and newline characters
+            ip_port_list.append(str(ip_port))
+    return ip_port_list
+
 if __name__ == '__main__':
-    if len(sys.argv) < 3:
+    if len(sys.argv) < 1:
         print('Usage: %s self_port partner1_addr partner2_addr ...' % sys.argv[0])
         sys.exit(-1)
-    #Discover my address, get function of the other repository that finds his own IP
     port = int(sys.argv[1])
-    partners = ['%s' % int(p) for p in sys.argv[2:]]
-    o = TestObj('localhost:%d' % port, partners)
+    #Discover my address, get function of the other repository that finds his own IP
+    allAddrs = []
+    #Lê-se endereços do ficheiro local
+    allAddrs = read_ip_port_file("nodes_addrs.txt")
+    print(f"All addresses: {allAddrs}")
+    selfAddr = ip_address_assign()
+    selfAddr = f"{selfAddr}:{port}"
+    selfAddr = selfAddr.strip()
+    print(f"Self Address: {selfAddr}")
+    if selfAddr:
+        allAddrs.remove(selfAddr)
+        addrs = allAddrs
+    #partners = ['%s' % int(p) for p in sys.argv[2:]]
+    o = TestObj(selfAddr, addrs)
     n = 0
     old_value = -1
     while True:
