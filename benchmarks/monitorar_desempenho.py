8b05b4f068cd2e6329b9ce3759ab6c1f83455fad
tuliosbhz <48711513+tuliosbhz@users.noreply.github.com>
2024-05-06 15:10:14 +0100

    Monitorador de desempenho modificado

--- benchmarks/monitorar_desempenho.py
+++ benchmarks/monitorar_desempenho.py
@@ -3,10 +3,30 @@ import speedtest
 import time
 import json
 import os
+
+def get_seconds_to_next_minute():
+    current_time = time.localtime()
+    seconds_remaining = 60 - current_time.tm_sec
+    return seconds_remaining
+
+
 def monitorar_desempenho():
     # Inicializar o Speedtest
-    teste_velocidade = speedtest.Speedtest()
+    try:
+        teste_velocidade = speedtest.Speedtest()
+    except Exception as e:
+        print(str(e))
+        while True: #Persiste em tentar adquirir um servidor para o teste de internet
+            teste_velocidade = speedtest.Speedtest()
+            time.sleep(10)
     while True:
+        # Calculate time until the next minute mark
+        seconds_to_wait = get_seconds_to_next_minute()
+        # Wait until the next minute mark
+        time.sleep(seconds_to_wait)
+        
+        # Get current timestamp
+        data_hora = time.strftime('%Y-%m-%d %H:%M:%S')
         # Obter o uso da CPU e da memória
         uso_cpu = psutil.cpu_percent()
         uso_memoria = psutil.virtual_memory().percent
@@ -15,9 +35,6 @@ def monitorar_desempenho():
         teste_velocidade.get_best_server()
         velocidade_upload = teste_velocidade.upload() / 1024 / 1024
         velocidade_download = teste_velocidade.download() / 1024 / 1024
-
-        # Obter a data e hora atual
-        data_hora = time.strftime("%Y-%m-%d %H:%M:%S")
 
         # Escrever os dados no arquivo
         results = {'Data hora':data_hora, 
@@ -34,10 +51,10 @@ def monitorar_desempenho():
             mode = "w"  # If the file doesn't exist, open it in write mode
         
         with open(filename, mode) as arquivo:
-            arquivo.write(json.dump(results))
-
-        # Esperar 1 minuto antes de realizar outra medição
-        time.sleep(60)
+            json.dump(results, arquivo)
+            arquivo.write('\n')
+            #arquivo.write(json.dump(results, "a"))
+        print(results)
 
 if __name__ == "__main__":
     monitorar_desempenho()
